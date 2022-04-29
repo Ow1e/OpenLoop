df0800b87affd973d4838268bf4ff2cf767f4281
Owen Shaule <55895503+Ow1e@users.noreply.github.com>
2022-04-29 16:17:12 -0400

    SQL Alerts

--- openloop/alerts.py
+++ openloop/alerts.py
@@ -1,30 +1,31 @@
+"""
+Manage alerts with db
+"""
+
+from datetime import datetime
+
 alert = """<a class="dropdown-item d-flex align-items-center" href="{}"><div class="me-3"><div class="bg-{} icon-circle"><i class="{} text-white"></i></div></div><div><span class="small text-gray-500">{}</span><p>{}</p></div></a>"""
 
-class Alert:
-    def __init__(self, link, color, icon, date, contents) -> None:
-        """Turns variables to formated text, represented as a string and a class"""
-        self.link, self.color, self.icon, self.date, self.contents = link, color, icon, date, contents
-        self.str_val = alert.format(link, color, icon, date, contents)
-
-    def export(self):
-        """Returns tuple of variables in order"""
-        return (self.link, self.color, self.icon, self.date, self.contents)
-
-class AlertManager(list):
+class AlertManager:
     """This is loaded by the loader, it has properties of a list but automatically sets a export function to ReFlow"""
     def __init__(self, shared):
-        super().__init__()
+        self.db = shared.db
+        self.alert = shared.database.Alert
+        alert = self.alert()
+        self.db.session.add(alert)
+        self.db.session.commit()
         shared.flow["alerts"] = {"inner": self.exp_html, "length": self.exp_len, "clear": self.clear}
         #self.append(Alert("#", "primary", "fas fa-tachometer-alt", "Today", "Wow it updated!!!!"))
 
+
     def exp_html(self):
-        p = ""
-        for i in self:
-            p += i.str_val
-        return p
+        contents = ""
+        for i in self.alert.query.all():
+            contents += i.export(alert)
+        return contents
 
     def exp_len(self):
-        x = len(self)
+        x = len(self.alert.query.all())
         if x == 0:
             x = ""
         elif x > 9:
@@ -32,4 +33,6 @@ class AlertManager(list):
         return x
 
     def clear(self):
-        super().__init__()
+        for i in self.alert.query.all():
+            self.db.session.delete(i)
+        self.db.session.commit()
--- openloop/database.py
+++ openloop/database.py
@@ -1,5 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from dateutil import tz
 from werkzeug.security import generate_password_hash, check_password_hash
+
+def div_exponent(inter, div):
+    running = True
+    current = 0
+    while running:
+        current += div
+        if current >= inter:
+            running = False
+            return (int((current-div)/div), inter-(current-div))
+    
+def convert_zones(datetype : datetime):
+    rel = datetype
+    now = datetime.utcnow()
+    change = now-rel
+    ans = ""
+    
+    minutes, seconds = div_exponent(change.seconds, 60)
+    hours, none = div_exponent(minutes, 60)
+    if minutes > 1 and hours == 0:
+        return f"{minutes} minutes ago."
+    elif minutes < 1 and hours == 0:
+        return f"{seconds} seconds ago."
+
 
 class Database:
     def __init__(self, shared) -> None:
@@ -7,6 +32,7 @@ class Database:
         shared.app.config["SQLALCHEMY_DATABASE_URI"] = settings.get("sqlalchemy_uri", "sqlite:///database.db")
         shared.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
         self.db = SQLAlchemy(shared.app)
+        self.models = self
         db = self.db
 
         class Users(db.Model):
@@ -23,4 +49,19 @@ class Database:
             def check_password(self, password):
                 return check_password_hash(self.password_hash, password)
 
+        class Alert(db.Model):
+            id = db.Column(db.Integer, primary_key=True)
+            link = db.Column(db.String(128), nullable = False, default="#")
+            color = db.Column(db.String(32), nullable = False, default="primary")
+            icon = db.Column(db.String(32), nullable = False, default="fas fa-box-open")
+            date = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+            contents = db.Column(db.Text, nullable = False, default="Text here")
+
+            def __repr__(self) -> str:
+                return f"{self.id}: {self.color}, {self.icon}, {self.date} --> {self.link}"
+
+            def export(self, html : str):
+                return html.format(self.link, self.color, self.icon, convert_zones(self.date), self.contents)
+
+        self.Alert = Alert
         self.Users = Users
