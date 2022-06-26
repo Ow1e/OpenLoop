328d4fbf1f4c59a760ff2368c6026e0c9b5ee3a0
Owen Shaule <owen.shaule@gmail.com>
2022-06-26 15:10:27 -0400

    Revert changes and add boot message

--- openloop/__init__.py
+++ openloop/__init__.py
@@ -4,6 +4,8 @@ st_code = "2"
 comb_code = str(num)+st_code
 lite_api = "v1"
 
+print(f"OpenLoop Core {num*10}:{st_code}")
+
 import subprocess
 def git_ver():
     x = subprocess.Popen(["git", "describe", "--tags"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
