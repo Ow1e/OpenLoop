e404c3b445be166acbea29df726636e9cc4fc8d6
Owen Shaule <owen.shaule@gmail.com>
2022-07-26 13:43:06 -0400

    Update version

--- openloop/__init__.py
+++ openloop/__init__.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 num = 0.3
-code = "Discovery"
-st_code = "4"
+code = "Marine"
+st_code = "5"
 comb_code = str(num)+st_code
 lite_api = "v1f"
 bootup = datetime.utcnow()
@@ -12,7 +12,7 @@ print(f"OpenLoop Core {num*10}:{st_code}")
 import subprocess
 def git_ver():
     try:
-        x = subprocess.Popen(["git", "describe", "--tags"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        x = subprocess.Popen(["git", "describe", "--tags"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         x.wait()
         if x.stderr.read() == b"":
             return x.stdout.read().decode().removesuffix("\n")
